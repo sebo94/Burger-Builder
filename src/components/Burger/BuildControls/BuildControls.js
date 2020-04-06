7bfd199fb94fe47db08f2ecb9a93456332c40692
sebo94 <sebastian.iuga94@gmail.com>
2020-04-06 18:16:41 +0200

    Initial Burger Builder commit

---
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -0,0 +1,30 @@
+import React from "react";
+import classes from "./BuildControls.module.css";
+import BuildControl from "./BuildControl/BuildControl";
+
+const controls = [
+  { label: "Salad", type: "salad" },
+  { label: "Bacon", type: "bacon" },
+  { label: "Cheese", type: "cheese" },
+  { label: "Meat", type: "meat" },
+];
+
+const buildControls = (props) => (
+  <div className={classes.BuildControls}>
+    <p>
+      Current Price: <strong> {props.price.toFixed(2)}</strong>
+    </p>
+    {controls.map((control) => (
+      <BuildControl
+        key={control.label}
+        label={control.label}
+        add={() => props.addIngredient(control.type)}
+        remove={() => props.removeIngredient(control.type)}
+        disabled={props.disabled[control.type]}
+      />
+    ))}
+    <button className={classes.OrderButton}>ORDER NOW</button>
+  </div>
+);
+
+export default buildControls;
---
+++ src/components/Burger/Burger.js
@@ -0,0 +1,27 @@
+import React from "react";
+import classes from "./Burger.module.css";
+import BurgerIngredient from "./BurgerIngredients/BurgerIngredient";
+
+const burger = (props) => {
+  let transformedIngredients = Object.keys(props.ingredients)
+    .map((ingredientKey) => {
+      return [...Array(props.ingredients[ingredientKey])].map((_, i) => (
+        <BurgerIngredient key={ingredientKey + i} type={ingredientKey} />
+      ));
+    })
+    .reduce((arr, element) => {
+      return arr.concat(element);
+    }, []);
+  if (transformedIngredients.length === 0) {
+    transformedIngredients = <p>Please start adding ingredients</p>;
+  }
+  return (
+    <div className={classes.Burger}>
+      <BurgerIngredient type="bread-top" />
+      {transformedIngredients}
+      <BurgerIngredient type="bread-bottom" />
+    </div>
+  );
+};
+
+export default burger;
