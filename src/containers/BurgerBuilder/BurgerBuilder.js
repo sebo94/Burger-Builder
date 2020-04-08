f712514bb971b7dfaeca4903764ab317c23cf1e8
sebo94 <sebastian.iuga94@gmail.com>
2020-04-08 19:14:10 +0200

    Added axios package

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,6 +4,7 @@ import Burger from "../../components/Burger/Burger";
 import BuildControls from "../../components/Burger/BuildControls/BuildControls";
 import Modal from "../../components/UI/Modal/Modal";
 import OrderSummary from "../../components/Burger/OrderSummary/OrderSummary";
+import axios from "../../axios-orders";
 
 const INGREDIENT_PRICES = {
   salad: 0.5,
@@ -74,8 +75,25 @@ class BurgerBuilder extends Component {
   purchaseCancelHandler = () => {
     this.setState({ purchasing: false });
   };
+
   purchaseContinueHanlder = () => {
-    alert("You continue!");
+    const order = {
+      ingredients: this.state.ingredients,
+      price: this.state.totalPrice,
+      customer: {
+        name: 'Zebbo Rebbo',
+        address: {
+          street: 'Backstreet 6',
+          zipCode: '32435',
+          country: 'Congo'
+        },
+        email: 'zebbo.rebbo@gmail.cum'
+      },
+      deliveryMethod: 'instant'
+    };
+    axios.post("/orders.json", order)
+    .then(response => console.log(response))
+    .catch(error => console.log(error));
   };
 
   render() {
